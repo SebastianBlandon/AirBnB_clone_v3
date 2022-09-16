1ce3f27fcb12cfa5b452dc60e1620c611f3857a2
Dontcode44 <kabutto378@gmail.com>
2022-09-16 12:06:42 -0500

    Add - Places 🎢

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -5,6 +5,7 @@
 """
 from models.city import City
 from models.place import Place
+from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
@@ -51,13 +52,22 @@ def post_place(city_id):
     city = storage.get(City, city_id)
     if not city:
         abort(404)
-    if not request.get_json():
+
+    data = request.get_json()
+    if not data:
         abort(400, description="Not a JSON")
+    if 'user_id' not in data:
+        abort(400, description="Missing user_id")
+
+    user = storage.get(User, data['user_id'])
+    if not user:
+        abort(404)
+
     if 'name' not in request.get_json():
         abort(400, description="Missing name")
-    data = request.get_json()
+
+    data['city_id'] = city_id
     instance = Place(**data)
-    instance.city_id = city_id
     instance.save()
     return make_response(jsonify(instance.to_dict()), 201)
 
@@ -70,7 +80,7 @@ def put_place(place_id):
         abort(404)
     if not request.get_json():
         abort(400, description="Not a JSON")
-    ignore = ['id', 'created_at', 'updated_at']
+    ignore = ['id', 'user_id', 'city_id', 'created_at', 'updated_at']
     data = request.get_json()
     for key, value in data.items():
         if key not in ignore:
